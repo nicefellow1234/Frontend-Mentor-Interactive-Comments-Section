f8db559cd62185d32a41388e9a8acd85dc08def9
Umair Shah <nicefellow1234@gmail.com>
2023-12-10 11:08:12 +0500

    Modal positioning on small screen devices fixed

--- src/app/modal.js
+++ src/app/modal.js
@@ -15,7 +15,7 @@ export default function Modal({
     >
       <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
       <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
-        <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+        <div className="flex min-h-full items-center justify-center p-4 text-center sm:p-0">
           <div className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-xs">
             <div className="bg-white px-3 pt-6">
               <div className="sm:flex sm:items-start">
